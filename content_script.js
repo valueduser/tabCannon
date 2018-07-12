65a42aa5c0c15acec12718efd73cc2b9ac95623b
Chris Lowe <clowe@luc.edu>
2018-07-11 22:12:52 -0500

    first commit

---
+++ content_script.js
@@ -0,0 +1,15 @@
+openAllHyperLinks();
+
+function openAllHyperLinks(){
+  let urls = [];
+
+  for(let i = document.links.length; i-->0;){
+    if (!urls.includes(document.links[i].href) && document.links[i].hostname !== location.href) {
+      urls.push(document.links[i].href);
+    }
+  }
+
+  for(let i = 5; i-->0;){
+    window.open(urls[i], '_blank');
+  }
+}
